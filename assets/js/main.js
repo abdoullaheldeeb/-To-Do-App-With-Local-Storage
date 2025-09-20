5a6eccf07c3d1c4797ca44063e3394035cfa6c35
Abdoullah ELdeeb <abdoullah.eldeeb@gmail.com>
2025-09-21 00:27:17 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,15 +12,14 @@ if (localStorage.getItem("tasks")) {
     arrayOFTasks = JSON.parse(localStorage.getItem("tasks"));
 }
 
-// Trigger Get Data Local Stroage Function
-getDataFromLocalStorage();
-
+// Trigger Get Data Local Storage Function
+updateUI();
 
 // Add Task
 submit.onclick = function () {
     if (input.value !== "") {
-        addTasksToArray(input.value); //Add Task To Array To Of Tasks
-        input.value = ""; //Empty Input Field
+        addTasksToArray(input.value); 
+        input.value = ""; 
     }
 }
 
@@ -28,21 +27,13 @@ submit.onclick = function () {
 tasksDiv.addEventListener("click", (e) => {
     // Delete Button 
     if (e.target.classList.contains("del")) {
-        // Remove Task from Local Storage 
         deleteTaskWith(e.target.parentElement.getAttribute("data-id"));
-        // Remove Element From Page 
-        e.target.parentElement.remove();
     }
     // task Element
     if (e.target.classList.contains("task")){
-        // Toggle Compalted For Task
         toggleStatusTaskWith(e.target.getAttribute("data-id"));
-        // toogle Done Class
-        e.target.classList.toggle("done");
     } 
 });
-
-
 
 function addTasksToArray(taskText){
     // Task Data
@@ -53,14 +44,8 @@ function addTasksToArray(taskText){
     };
     // Push task To Array Of Tasks
     arrayOFTasks.push(task);
-    // console.log(arrayOFTasks);
-    // Add Taks To Page 
-    addElementsToPageFrom(arrayOFTasks);
-    // Add Tasks p Local Storage
     addDateToLocalStorageFrom(arrayOFTasks);
-    // For Testing 
-    // console.log(arrayOFTasks);
-    // console.log(JSON.stringify(arrayOFTasks));
+    updateUI();
 }
 
 
@@ -86,58 +71,65 @@ function addElementsToPageFrom(arrayOFTasks){
         div.appendChild(span);
         // Add task Div TO tasks Container
         tasksDiv.appendChild(div);
-        
     });
-
 }
+
 
 function addDateToLocalStorageFrom(arrayOFTasks){
     window.localStorage.setItem("tasks", JSON.stringify(arrayOFTasks));
-    // Add input all del
-    
 }
-
 
 
 function getDataFromLocalStorage() {
     let data = window.localStorage.getItem("tasks");
     if (data) {
         let tasks = JSON.parse(data);
+        arrayOFTasks = tasks; 
         addElementsToPageFrom(tasks);
-        
     }
 }
 
 
 function deleteTaskWith(taskId) {
-    // for Explain Only
-    // for (let i = 0; i < arrayOFTasks.length; i++){
-    //     console.log(`${arrayOFTasks[i].id} === ${taskId}`);
-    // }
     arrayOFTasks = arrayOFTasks.filter((task) => task.id != taskId);
     addDateToLocalStorageFrom(arrayOFTasks);
+    updateUI();
 }
 
 
 function toggleStatusTaskWith(taskId) {
     for (let i = 0; i < arrayOFTasks.length; i++){
         if (arrayOFTasks[i].id == taskId ) {
-            arrayOFTasks[i].completed == false ? (arrayOFTasks[i].completed = true) : (arrayOFTasks[i].completed = false)
+            arrayOFTasks[i].completed == false 
+              ? (arrayOFTasks[i].completed = true) 
+              : (arrayOFTasks[i].completed = false);
         }
     }
     addDateToLocalStorageFrom(arrayOFTasks);
+    updateUI();
 }
 
-// Append Button clear All
-let clearAllBtn = document.createElement("button");
-clearAllBtn.className = "del";
-clearAllBtn.appendChild(document.createTextNode("AllDelete"));
-container.appendChild(clearAllBtn)
-// clear All Btn
-clearAllBtn.onclick = function () {
-    arrayOFTasks = []; 
-    localStorage.removeItem("tasks"); 
-    tasksDiv.innerHTML = ""; 
-    clearAllBtn.remove();
-};
 
+function updateUI() {
+    addElementsToPageFrom(arrayOFTasks);
+
+    // remove delete old clear-all button 
+    let oldBtn = document.querySelector(".clear-all");
+    if (oldBtn) oldBtn.remove();
+
+    // Add button of tasks
+    if (arrayOFTasks.length > 0) {
+        let clearAllBtn = document.createElement("button");
+        clearAllBtn.className = "clear-all del";
+        clearAllBtn.appendChild(document.createTextNode("AllDelete"));
+        container.appendChild(clearAllBtn);
+
+        // clearAllBtn onclick
+        clearAllBtn.onclick = function () {
+            arrayOFTasks = []; 
+            addDateToLocalStorageFrom(arrayOFTasks); 
+            tasksDiv.innerHTML = ""; 
+            clearAllBtn.remove();
+        };
+    }
+}
